3598e44a0e04c15ff1dfcf6db5a3e0b129895da9
webtrifonov <webtrifonov@gmail.com>
2024-07-07 11:29:04 +0300

    gh actions

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./.env
 
-docker-compose -f ./docker-compose.yaml down
+docker compose -f ./docker-compose.yaml down
 docker system prune -f --volumes
-docker-compose -f ./docker-compose.yaml up -d --build
+docker compose -f ./docker-compose.yaml up -d --build
 docker logs -f $BACKEND_APP_NAME"_node-api"
